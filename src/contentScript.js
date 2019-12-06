0e0e9530ab1313da061e8e7c8408ebc71e35e55b
Nguyễn Vũ <pika.nguyenvu@gmail.com>
2019-12-06 07:21:50 +0700

    Init project

--- src/contentScript.js
+++ src/contentScript.js
@@ -1,43 +1,28 @@
 'use strict';
 
-// Content script file will run in the context of web page.
-// With content script you can manipulate the web pages using
-// Document Object Model (DOM).
-// You can also pass information to the parent extension.
 
-// We execute this script by making an entry in manifest.json file
-// under `content_scripts` property
+function getListComment() {
+  var listComments = $("._6ssm > a._3_qk._6ssj._42ft + ul > li");
+  return listComments.map( (index, comment) => {
+    var commentRaw = {};
+    commentRaw.name = $(comment).find("a._6svv")[0].innerHTML;
+    commentRaw.avatar = $(comment).find("img._3me-._3mf2.img").attr("src");
+    commentRaw.profileUrl = $(comment).find("a._6svv").attr("href");
+    commentRaw.profileId = commentRaw.profileUrl.match(/id=([^&]+)/)[1];
+    commentRaw.timeString = $(comment).find("a._6svs > abbr").data("tooltip-content");
+    commentRaw.timeAgo = $(comment).find("span.timestampContent")[0].innerHTML;
+    commentRaw.shortComment = $(comment).find("span._3l3x._1n4g")[0].innerHTML;
 
-// For more information on Content Scripts,
-// See https://developer.chrome.com/extensions/content_scripts
-
-// Log `title` of current active web page
-const pageTitle = document.head.getElementsByTagName('title')[0].innerHTML;
-console.log(
-  `Page title is: '${pageTitle}' - evaluated by Chrome extension's 'contentScript.js' file`
-);
-
-// Communicate with background file by sending a message
-chrome.runtime.sendMessage(
-  {
-    type: 'GREETINGS',
-    payload: {
-      message: 'Hello, my name is Con. I am from ContentScript.',
-    },
-  },
-  response => {
-    console.log(response.message);
-  }
-);
+    return commentRaw;
+  });
+}
 
 // Listen for message
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.type === 'COUNT') {
-    console.log(`Current count is ${request.payload.count}`);
-  }
-
-  // Send an empty response
-  // See https://github.com/mozilla/webextension-polyfill/issues/130#issuecomment-531531890
-  sendResponse({});
+  if (request.type === 'GET_LIST_COMMENT') {
+    var listComments = getListComment();
+    sendResponse({listComments});
+    return true;
+  };
   return true;
 });
--- src/popup.js
+++ src/popup.js
@@ -2,111 +2,193 @@
 
 import './popup.css';
 
-(function() {
-  // We will make use of Storage API to get and store `count` value
-  // More information on Storage API can we found at
-  // https://developer.chrome.com/extensions/storage
-
-  // To get storage access, we have to mention it in `permissions` property of manifest.json file
-  // More information on Permissions can we found at
-  // https://developer.chrome.com/extensions/declare_permissions
-  const counterStorage = {
-    get: cb => {
-      chrome.storage.sync.get(['count'], result => {
-        cb(result.count);
-      });
-    },
-    set: (value, cb) => {
-      chrome.storage.sync.set(
-        {
-          count: value,
-        },
-        () => {
-          cb();
-        }
-      );
-    },
-  };
-
-  function setupCounter(initialValue = 0) {
-    document.getElementById('counter').innerHTML = initialValue;
-
-    document.getElementById('incrementBtn').addEventListener('click', () => {
-      updateCounter({
-        type: 'INCREMENT',
-      });
-    });
-
-    document.getElementById('decrementBtn').addEventListener('click', () => {
-      updateCounter({
-        type: 'DECREMENT',
-      });
-    });
+var teamMembers = [
+  {
+    "id": "100042708771710",
+    "name": "Trần Thanh Tùng"
+  },
+  {
+    "id": "100042215024047",
+    "name": "Luong Quang Anh"
+  },
+  {
+    "id": "100041792864833",
+    "name": "Phạm Thị Hiền"
+  },
+  {
+    "id": "100040973031347",
+    "name": "Bùi Văn Chương"
+  },
+  {
+    "id": "100041324840566",
+    "name": "Phạm Văn Dũng"
+  },
+  {
+    "id": "100041483172772",
+    "name": "Đặng Tuấn Mạnh"
+  },
+  {
+    "id": "100041504562480",
+    "name": "Pham Ngoc MInh"
+  },
+  {
+    "id": "100039206090014",
+    "name": "Nguyễn Văn Vũ"
+  },
+  {
+    "id": "100038780143367",
+    "name": "Hoàng Thị Thảo"
+  },
+  {
+    "id": "100038111835489",
+    "name": "Hoàng Quốc Huy"
+  },
+  {
+    "id": "100035584371123",
+    "name": "Nguyễn Đức Thắng"
+  },
+  {
+    "id": "100035521240354",
+    "name": "Nguyễn Thị Huyền Trang"
+  },
+  {
+    "id": "100034734091665",
+    "name": "Phan Hoàng Long"
+  },
+  {
+    "id": "100033040152590",
+    "name": "Nguyễn Tiến Anh"
+  },
+  {
+    "id": "100029217756538",
+    "name": "Nguyễn Trường Giang"
+  },
+  {
+    "id": "100028653420382",
+    "name": "Đào Mạnh Tuấn"
+  },
+  {
+    "id": "100028470545326",
+    "name": "Nguyễn Thị Lệ"
+  },
+  {
+    "id": "100028061796140",
+    "name": "Nguyễn Sỹ Tuấn"
+  },
+  {
+    "id": "100023565023183",
+    "name": "Đặng Đình Dương"
+  },
+  {
+    "id": "100025593033330",
+    "name": "Nguyễn Xuân Tú"
+  },
+  {
+    "id": "100025582625198",
+    "name": "Hữu Hưởng Nguyễn"
+  },
+  {
+    "id": "100022122040233",
+    "name": "Đào Quang Linh"
+  },
+  {
+    "id": "100021881294227",
+    "name": "Hà Thanh Sơn"
   }
+];
 
-  function updateCounter({ type }) {
-    counterStorage.get(count => {
-      let newCount;
+function exportExcel() {
+      var membersWs = XLSX.utils.json_to_sheet(teamMembers);
+      var wb = XLSX.utils.book_new();
+      XLSX.utils.book_append_sheet(wb, membersWs, 'Thành viên trong team');  
 
-      if (type === 'INCREMENT') {
-        newCount = count + 1;
-      } else if (type === 'DECREMENT') {
-        newCount = count - 1;
-      } else {
-        newCount = count;
-      }
+      // export Excel file
+      XLSX.writeFile(wb, 'team-members.xlsx');
+    
+};
 
-      counterStorage.set(newCount, () => {
-        document.getElementById('counter').innerHTML = newCount;
+function getListComments() {
+    // active tab by sending a message
+  chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+    const tab = tabs[0];
 
-        // Communicate with content script of
-        // active tab by sending a message
-        chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
-          const tab = tabs[0];
+    chrome.tabs.sendMessage(tab.id,{ type: 'GET_LIST_COMMENT'},response => {
 
-          chrome.tabs.sendMessage(
-            tab.id,
-            {
-              type: 'COUNT',
-              payload: {
-                count: newCount,
-              },
-            },
-            response => {
-              console.log('Current count value passed to contentScript file');
-            }
-          );
-        });
-      });
     });
-  }
+  });
+};
 
-  function restoreCounter() {
-    // Restore count value
-    counterStorage.get(count => {
-      if (typeof count === 'undefined') {
-        // Set counter value as 0
-        counterStorage.set(0, () => {
-          setupCounter(0);
-        });
-      } else {
-        setupCounter(count);
-      }
-    });
-  }
+$(document).ready(() => {
+  $("#btnGetListComment").click(() => {
+    exportExcel();
+  })
+})
 
-  document.addEventListener('DOMContentLoaded', restoreCounter);
+  // function setupCounter(initialValue = 0) {
+  //   document.getElementById('counter').innerHTML = initialValue;
 
-  // Communicate with background file by sending a message
-  chrome.runtime.sendMessage(
-    {
-      type: 'GREETINGS',
-      payload: {
-        message: 'Hello, my name is Pop. I am from Popup.',
-      },
-    },
-    response => {
-      console.log(response.message);
-    }
-  );
-})();
+  //   document.getElementById('incrementBtn').addEventListener('click', () => {
+  //     updateCounter({
+  //       type: 'INCREMENT',
+  //     });
+  //   });
+
+  //   document.getElementById('decrementBtn').addEventListener('click', () => {
+  //     updateCounter({
+  //       type: 'DECREMENT',
+  //     });
+  //   });
+  // }
+
+  // function updateCounter({ type }) {
+  //   counterStorage.get(count => {
+  //     let newCount;
+
+  //     if (type === 'INCREMENT') {
+  //       newCount = count + 1;
+  //     } else if (type === 'DECREMENT') {
+  //       newCount = count - 1;
+  //     } else {
+  //       newCount = count;
+  //     }
+
+  //     counterStorage.set(newCount, () => {
+  //       document.getElementById('counter').innerHTML = newCount;
+
+  //       // Communicate with content script of
+  //       // active tab by sending a message
+  //       chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+  //         const tab = tabs[0];
+
+  //         chrome.tabs.sendMessage(
+  //           tab.id,
+  //           {
+  //             type: 'COUNT',
+  //             payload: {
+  //               count: newCount,
+  //             },
+  //           },
+  //           response => {
+  //             console.log('Current count value passed to contentScript file');
+  //           }
+  //         );
+  //       });
+  //     });
+  //   });
+  // }
+
+  // function restoreCounter() {
+  //   // Restore count value
+  //   counterStorage.get(count => {
+  //     if (typeof count === 'undefined') {
+  //       // Set counter value as 0
+  //       counterStorage.set(0, () => {
+  //         setupCounter(0);
+  //       });
+  //     } else {
+  //       setupCounter(count);
+  //     }
+  //   });
+  // }
+
+  // document.addEventListener('DOMContentLoaded', restoreCounter);
